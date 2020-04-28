f7492ea25c1b2ffdd42fd0e4f24844df81145ca1
Vinayak Yuvraj Bhatt <vinayak.bhatt@habilelabs.io>
2020-04-28 13:57:23 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./App.css";
+
+
+function  App ( )  {
+    const [count, setCount] = React.useState(0);
+
+    const increment = () => {
+        setCount(count + 1);
+    };
+
+    const decrement = () => {
+        setCount(count - 1);
+    };
+
+    return (
+        <>
+        <img src="./logos.svg" className="App-logo" alt="logo" />
+        <p>{count}</p>
+        <button onClick={increment}>Increment</button>
+        <button onClick={decrement}>Decrement</button>
+        </>
+    );
+}
+
+export default App;
