6b5b2c623337604669978aa00c169c1eb1900f63
Vinayak Yuvraj Bhatt <vinayak.bhatt@habilelabs.io>
2020-04-30 12:19:57 +0530

    middleware added for /logo /home

--- server/server.js
+++ server/server.js
@@ -36,6 +36,97 @@ app.use('^/$', (req, res, next) => {
         const indexFile = path.resolve('./build/index.html');
         fs.readFile(indexFile, 'utf8', (err, indexData) => {
             try {
+                if (context.status === 404) {
+                    res.status(404);
+                }
+                if (context.url) {
+                    return res.redirect(301, context.url);
+                }
+
+                return res.send(
+                    indexData
+                        .replace('<div id="root"></div>', `<div id="root">${app}</div>`)
+                        .replace(
+                            '</body>',
+                            `<script>window.__ROUTE_DATA__ = ${serialize(data)}</script></body>`
+                        )
+                );
+            }
+            catch(err){
+                if (err) {
+                    console.error('Something went wrong:', err);
+                    return res.status(500).send('Oops, better luck next time!');
+                }
+            }
+        });
+    });
+});
+app.use('^/logo', (req, res, next) => {
+    const currentRoute =
+        Routes.find(route => matchPath(req.url, route)) || {};
+    let promise;
+    if (currentRoute.loadData) {
+        promise = currentRoute.loadData();
+    } else {
+        promise = Promise.resolve(null);
+    }
+    promise.then(data => {
+        const context = {};
+        const app = ReactDOMServer.renderToString(
+            <StaticRouter location={req.url} context={context}>
+                <App/>
+            </StaticRouter>
+        );
+        const indexFile = path.resolve('./build/index.html');
+        fs.readFile(indexFile, 'utf8', (err, indexData) => {
+            try {
+
+
+                if (context.status === 404) {
+                    res.status(404);
+                }
+                if (context.url) {
+                    return res.redirect(301, context.url);
+                }
+
+                return res.send(
+                    indexData
+                        .replace('<div id="root"></div>', `<div id="root">${app}</div>`)
+                        .replace(
+                            '</body>',
+                            `<script>window.__ROUTE_DATA__ = ${serialize(data)}</script></body>`
+                        )
+                );
+            }
+            catch(err){
+                if (err) {
+                    console.error('Something went wrong:', err);
+                    return res.status(500).send('Oops, better luck next time!');
+                }
+            }
+        });
+    });
+});
+app.use('^/home', (req, res, next) => {
+    const currentRoute =
+        Routes.find(route => matchPath(req.url, route)) || {};
+    let promise;
+    if (currentRoute.loadData) {
+        promise = currentRoute.loadData();
+    } else {
+        promise = Promise.resolve(null);
+    }
+    promise.then(data => {
+        console.log({data});
+        const context = {};
+        const app = ReactDOMServer.renderToString(
+            <StaticRouter location={req.url} context={context}>
+                <App/>
+            </StaticRouter>
+        );
+        const indexFile = path.resolve('./build/index.html');
+        fs.readFile(indexFile, 'utf8', (err, indexData) => {
+            try {
 
 
                 if (context.status === 404) {
@@ -65,6 +156,8 @@ app.use('^/$', (req, res, next) => {
 });
 
 app.use(express.static(path.resolve(__dirname, '..', 'build')));
+// app.use(express.static(__dirname , '..',  'build'));
+console.log(__dirname, "((((",path.resolve(__dirname, '..', 'build'));
 app.listen(PORT, () => {
     console.log(`App launched on ${PORT}`)
 });
